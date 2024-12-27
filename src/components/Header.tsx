12cb4eabc176db9870bcc414b8776849d0fbef9b
Daniel Escoto <escotodanny62@gmail.com>
2024-12-27 14:09:03 -0500

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <header className="text-center mb-6">
-      <h1 className="text-3xl font-bold">Fire Truck Invoice Generator</h1>
+      <h1 className="text-xl/normal md:text-2xl/normal font-medium text-black dark:text-white">
+        Fire Truck Invoice Generator
+      </h1>
     </header>
   );
 }
